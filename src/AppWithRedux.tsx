4f2f6aa06f1cac4006cf8c641fe615c8ce2aa6f0
MaAnka324 <matviychuk.anna324@gmail.com>
2023-08-01 19:34:16 +0300

    Change README.md

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -24,6 +24,8 @@ import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 import {ErrorSnackbar} from "./components/ErrorSnackbar/ErrorSnackbar";
 import {RequestStatusType} from "./app/app-reducer";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Login} from "./features/Login";
 
 
 export type TasksStateType = {
@@ -101,28 +103,36 @@ function AppWithRedux(): JSX.Element {
     const status = useAppSelector<RequestStatusType>(state => state.app.status)
 
     return (
-        <div className="App">
-            <ErrorSnackbar/>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu/>
-                    </IconButton>
-                    <Typography variant="h6">
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-            {status === 'loading'
-                && <Box sx={{width: '100%'}}>
-                    <LinearProgress/>
-                </Box>}
-            <AddItemForm maxLengthUserName={15} addItem={addTodolist} className='addItemForm'/>
-            <div className={'todolists'}>
-                {todoListsComponents}
+        <BrowserRouter>
+            <div className="App">
+                <ErrorSnackbar/>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <Menu/>
+                        </IconButton>
+                        <Typography variant="h6">
+                            News
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+
+                {/*<Switch>*/}
+                {/*    <Route path='/' element={<div/>}/>*/}
+                {/*    <Route path='/path' element={<Login/>}/>*/}
+                {/*</Switch>*/}
+
+                {status === 'loading'
+                    && <Box sx={{width: '100%'}}>
+                        <LinearProgress/>
+                    </Box>}
+                <AddItemForm maxLengthUserName={15} addItem={addTodolist} className='addItemForm'/>
+                <div className={'todolists'}>
+                    {todoListsComponents}
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     )
 }
 
